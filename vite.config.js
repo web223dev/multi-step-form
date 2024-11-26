ca22754b0286173a498d9e600c355b58e4568610
web223dev <ruslanus866@outlook.com>
2024-11-27 01:04:11 +0800

    install Shadcn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
