c564e280ec055078a6bb2ae17e80eced5e4d539d
web223dev <ruslanus866@outlook.com>
2024-11-27 02:39:32 +0800

    Add context API

--- src/components/step-item.tsx
+++ src/components/step-item.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useMultiContext } from "@/contexts/multistep-form-context";
+
 interface StepItemProps {
   infos: {
     num: number;
@@ -6,10 +9,16 @@ interface StepItemProps {
   };
 }
 export function StepItem({ infos }: StepItemProps) {
+  const { step } = useMultiContext();
+  const isFinished = step === 5 && infos.num === 4;
   return (
     <li className="flex items-center gap-4 uppercase">
       <span
-        className={`flex size-10 items-center justify-center rounded-full border-2 font-medium`}
+        className={`flex size-10 items-center justify-center rounded-full border-2 font-medium ${
+          step === infos.num || isFinished
+            ? "border-indigo-300 bg-indigo-300 text-black"
+            : ""
+        }`}
       >
         {infos.num}
       </span>
--- src/components/steps.tsx
+++ src/components/steps.tsx
@@ -8,7 +8,7 @@ const stepData = [
 ];
 export function Steps() {
   return (
-    <header className="flex h-[200px] items-start justify-center bg-sidebarMobile bg-cover bg-no-repeat p-6 lg:row-span-3 lg:row-start-2 lg:h-[540px] lg:justify-start lg:rounded-md lg:bg-sidebarDesktop lg:bg-cover lg:bg-center lg:pt-10">
+    <header className="bgno-repeat flex h-[200px] items-start justify-center bg-blue-500 bg-sidebarMobile bg-cover p-6 lg:row-span-3 lg:row-start-2 lg:h-[540px] lg:justify-start lg:rounded-md lg:bg-sidebarDesktop lg:bg-cover lg:bg-center lg:pt-10">
       <ul className="flex gap-4 text-white lg:flex-col lg:gap-8">
         {stepData.map((info, index) => {
           return <StepItem key={index} infos={info} />;
